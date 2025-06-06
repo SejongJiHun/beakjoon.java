81f7a71acf2e1be3602d48b7137abb714b5a5196
sejong_jihun <136591728+SejongJiHun@users.noreply.github.com>
2025-06-06 10:50:09 +0900

    p11723.java

---
+++ src/p11723.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));;
+
+    static int [] S = new int[21];
+
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        int M = Integer.parseInt(br.readLine());
+
+
+        for(int i = 0; i < M; i++) {
+            String[] input = br.readLine().split(" ");
+            String command = input[0];
+            if(command.equals("add")){
+                int x = Integer.parseInt(input[1]);
+                if(S[x] == 0) {
+                    S[x] = 1;
+                }
+            }
+            else if (command.equals("remove")) {
+                int x = Integer.parseInt(input[1]);
+                if(S[x] == 1) {
+                    S[x] = 0;
+                }
+            }
+            else if (command.equals("check")) {
+                int x = Integer.parseInt(input[1]);
+                if(S[x] == 1) {
+                    bw.write("1\n");
+                }
+                else if(S[x] == 0) {
+                    bw.write("0\n");
+                }
+            }
+            else if(command.equals("toggle")) {
+                int x = Integer.parseInt(input[1]);
+                if(S[x] == 1) {
+                    S[x] = 0;
+                }
+                else if(S[x] == 0) {
+                    S[x] = 1;
+                }
+            }
+            else if(command.equals("all")) {
+                for(int j = 1; j <= 20; j++){
+                    S[j] = 1;
+                }
+            }
+            else if(command.equals("empty")) {
+                for(int j = 1; j <= 20; j++){
+                    S[j] = 0;
+                }
+            }
+
+        }
+
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+
+
+
+}
